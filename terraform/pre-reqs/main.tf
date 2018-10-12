f30c212d8f994a2b70458f435219c16e2591d572
Steve Winwood <stevewinwood@Steves-Work-MacBook-Pro.local>
2018-10-12 11:24:42 +0100

    Adding VPC config

---
+++ terraform/pre-reqs/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "eu-west-2"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "ECS-example"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  assign_generated_ipv6_cidr_block = false
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Owner       = "Steve"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "ECS-example"
+  }
+}
---
+++ terraform/pre-reqs/vars.tf
@@ -0,0 +1,6 @@
+variable "aws_access_key" {
+    description = "Provide Access Key ID for IAM user"
+}
+variable "aws_secret_key" {
+    description = "Provide Secret Access Key for IAM user"
+}
